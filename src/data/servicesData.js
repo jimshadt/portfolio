237f691855b8167099f6eba38bbd628422763e9a
Jimshad <jimshaddb@gmail.com>
2022-08-04 13:53:16 +0530

    servicesData updated

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
-import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
-import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+// import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
+// import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
+import { FaCameraRetro, FaVideo, FaCode, FaPaintBrush, FaLaptopCode } from "react-icons/fa";
 
  
 
@@ -14,9 +14,9 @@ export const servicesData = [
     // },
     // {
     //     id: 2,
-    //     title: 'Content Writing',
-    //     icon: <BiPencil /> 
-    // },
+    //     title: 'Pinterest Virtual Assistant',
+    //     icon: <FaPinterest />
+    // }, 
     // {
     //     id: 3,
     //     title: 'Email Marketing',
@@ -24,30 +24,29 @@ export const servicesData = [
     // },
     {
         id: 4,
-        title: 'Internet Research',
-        icon: <FaInternetExplorer />
+        title: 'Web development',
+        icon: <FaCode />
     },
-    // {
-    //     id: 5,
-    //     title: 'Tutoring',
-    //     icon: <FaChalkboardTeacher />
-    // },
-
+    {
+        id: 5,
+        title: 'Web Design',
+        icon: <FaLaptopCode />
+    },
     {
         id: 6,
-        title: 'Web Design',
-        icon: <BsCodeSlash />
-    },
-    {
-        id: 7,
-        title: 'Photography',
-        icon: <FaCameraRetro />
+        title: 'Graphic Design',
+        icon: <FaPaintBrush/>
     },
     // {
-    //     id: 8,
-    //     title: 'Pinterest Virtual Assistant',
-    //     icon: <FaPinterest />
-    // }, 
+    //     id: 7,
+    //     title: 'Photography',
+    //     icon: <FaCameraRetro />
+    // },
+    {
+        id: 8,
+        title: 'Content Writing',
+        icon: <BiPencil /> 
+    },
     {
         id: 9,
         title: 'Video Editing',
@@ -58,11 +57,11 @@ export const servicesData = [
     //     title: 'Data Entry', 
     //     icon: <BsClipboardData />
     // },
-    {
-        id: 11,
-        title: 'Ebook Writing',
-        icon: <FaTabletAlt />
-    },
+    // {
+    //     id: 11,
+    //     title: 'Ebook Writing',
+    //     icon: <FaTabletAlt />
+    // },
     // {
     //     id: 12,
     //     title: 'Audio Transcription',
@@ -77,5 +76,4 @@ export const servicesData = [
 ]
 
 // Uncomment your required service.
-// Couldn't find the required services? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
 // You can also add on your own 😉.
